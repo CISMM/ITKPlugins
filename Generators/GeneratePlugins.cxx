37e2489bbe05ffe03df24943088058b1867a8e77
Cory Quammen <cquammen@cs.unc.edu>
2012-03-19 14:32:03 -0400

    JSON configuration file is now read as a command line argument.

--- Generators/GeneratePlugins.cxx
+++ Generators/GeneratePlugins.cxx
@@ -9,11 +9,19 @@
 
 int main( int argc, char* argv[] )
 {
+
+  if ( argc <= 0 )
+    {
+    std::cout << "Usage: " << argv[0] << " <plugin JSON file>" << std::endl;
+
+    return EXIT_FAILURE;
+    }
+
   ClassDescription *description = new ClassDescription();
 
   // Parse the class description
   Parser parser;
-  parser.SetFileName( "/Users/quammen/dev/packages/SimpleITK/Code/BasicFilters/json/AddConstantTo.json" );
+  parser.SetFileName( argv[1] );
   parser.Parse( description );
 
   description->PrintSelf( std::cout );
