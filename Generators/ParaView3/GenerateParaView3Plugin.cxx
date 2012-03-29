5fbc00c02fcd09ac908e8659312eee68e875721e
Cory Quammen <cquammen@cs.unc.edu>
2012-03-29 15:18:16 -0400

    Consolodated file generation into one custom command.
    Changed the generation program to just produce all generated files
    instead of taking a command line argument determining which file to
    generate. This exploits CMake's add_custom_command command to specify
    more than one output.

--- Generators/ParaView3/GenerateParaView3Plugin.cxx
+++ Generators/ParaView3/GenerateParaView3Plugin.cxx
@@ -10,9 +10,9 @@
 int main( int argc, char* argv[] )
 {
 
-  if ( argc < 4 )
+  if ( argc < 3 )
     {
-    std::cout << "Usage: " << argv[0] << " <plugin JSON file> <output file prefix> [ --xml | --cxx ]" << std::endl;
+    std::cout << "Usage: " << argv[0] << " <plugin JSON file> <output file prefix> " << std::endl;
 
     return EXIT_FAILURE;
     }
@@ -27,30 +27,18 @@ int main( int argc, char* argv[] )
   description->PrintSelf( std::cout );
 
   // Generate a Slicer 4 plugin from this description
-  ParaView3PluginGenerator * slicer4Generator = new ParaView3PluginGenerator();
-  slicer4Generator->SetClassDescription( description );
-  slicer4Generator->SetPluginPath( argv[2] );
+  ParaView3PluginGenerator * paraview3Generator = new ParaView3PluginGenerator();
+  paraview3Generator->SetClassDescription( description );
+  paraview3Generator->SetPluginPath( argv[2] );
 
-  if ( strncmp( "--xml", argv[3], 6 ) == 0 )
+  bool success = paraview3Generator->Generate();
+  if ( !success )
     {
-    bool success = slicer4Generator->GenerateXML();
-    if ( !success )
-      {
-      std::cerr << "Error generating XML" << std::endl;
-      return EXIT_FAILURE;
-      }
-    }
-  else if ( strncmp( "--cxx", argv[3], 6 ) == 0 )
-    {
-    bool success = slicer4Generator->GenerateCode();
-    if ( !success )
-      {
-      std::cerr << "Error generating CXX" << std::endl;
-      return EXIT_FAILURE;
-      }
+    std::cerr << "Error generating source files!" << std::endl;
+    return EXIT_FAILURE;
     }
 
-  delete slicer4Generator;
+  delete paraview3Generator;
   delete description;
 
   return EXIT_SUCCESS;
