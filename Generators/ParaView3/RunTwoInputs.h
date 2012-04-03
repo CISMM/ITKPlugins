ccb4abdcb90d5a59f7c47e5668e2481f7b63df0a
Cory Quammen <cquammen@cs.unc.edu>
2012-04-03 17:45:51 -0400

    Added conditional compilation in switch statement over pixel types.

--- Generators/ParaView3/RunTwoInputs.h
+++ Generators/ParaView3/RunTwoInputs.h
@@ -27,7 +27,67 @@ if (!output)
 
 switch ( input0->GetScalarType() )
   {
-  vtkReducedTypeTemplateMacro( DispatchTwoInputs( static_cast< VTK_TT *>( 0 ), input0, input1, output ) );
+#ifdef ITK_UCHAR_TYPE
+  case VTK_UNSIGNED_CHAR:
+    this->Run( static_cast< unsigned char * >( 0 ), static_cast< unsigned char * >( 0 ),
+              input0, input1, output );
+    break;
+#endif
+#ifdef ITK_CHAR_TYPE
+  case VTK_CHAR:
+  case VTK_SIGNED_CHAR:
+    this->Run( static_cast< char * >( 0 ), static_cast< char * >( 0 ),
+              input0, input1, output );
+    break;
+#endif
+#ifdef ITK_USHORT_TYPE
+  case VTK_UNSIGNED_SHORT:
+    this->Run( static_cast< unsigned short * >( 0 ), static_cast< unsigned short * >( 0 ),
+              input0, input1, output );
+    break;
+#endif
+#ifdef ITK_SHORT_TYPE
+  case VTK_SHORT:
+    this->Run( static_cast< short * >( 0 ), static_cast< short * >( 0 ),
+              input0, input1, output );
+    break;
+#endif
+#ifdef ITK_UINT_TYPE
+  case VTK_UNSIGNED_INT:
+    this->Run( static_cast< unsigned int * >( 0 ), static_cast< unsigned int * >( 0 ),
+              input0, input1, output );
+    break;
+#endif
+#ifdef ITK_INT_TYPE
+  case VTK_INT:
+    this->Run( static_cast< int * >( 0 ), static_cast< int * >( 0 ),
+              input0, input1, output );
+    break;
+#endif
+#ifdef ITK_ULONG_TYPE
+  case VTK_UNSIGNED_LONG:
+    this->Run( static_cast< unsigned long * >( 0 ), static_cast< unsigned long * >( 0 ),
+              input0, input1, output );
+    break;
+#endif
+#ifdef ITK_LONG_TYPE
+  case VTK_LONG:
+    this->Run( static_cast< long * >( 0 ), static_cast< long * >( 0 ),
+              input0, input1, output );
+    break;
+#endif
+#ifdef ITK_FLOAT_TYPE
+  case VTK_FLOAT:
+    this->Run( static_cast< float * >( 0 ), static_cast< float * >( 0 ),
+              input0, input1, output );
+    break;
+#endif
+#ifdef ITK_DOUBLE_TYPE
+  case VTK_DOUBLE:
+    this->Run( static_cast< double * >( 0 ), static_cast< double * >( 0 ),
+              input0, input1, output );
+    break;
+#endif
 
   default:
     vtkErrorMacro( "Unknown pixel type encountered in input0" );
