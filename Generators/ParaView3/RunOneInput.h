041a3074a8653a0a8b6e0c5b8280f4f275c76172
Cory Quammen <cquammen@cs.unc.edu>
2012-04-03 17:44:11 -0400

    Added conditional compilation in switch statement over pixel types.

--- Generators/ParaView3/RunOneInput.h
+++ Generators/ParaView3/RunOneInput.h
@@ -16,27 +16,61 @@ if (!output)
   return 0;
   }
 
-#define vtkReducedTypeTemplateMacro(call)                                   \
-  vtkTemplateMacroCase(VTK_DOUBLE, double, call);                           \
-  vtkTemplateMacroCase(VTK_FLOAT, float, call);                             \
-  //vtkTemplateMacroCase_ll(VTK_LONG_LONG, long long, call)             \
-  //vtkTemplateMacroCase_ll(VTK_UNSIGNED_LONG_LONG, unsigned long long, call) \
-  vtkTemplateMacroCase_si64(VTK___INT64, __int64, call)                     \
-  vtkTemplateMacroCase_ui64(VTK_UNSIGNED___INT64, unsigned __int64, call)   \
-  //vtkTemplateMacroCase(VTK_ID_TYPE, vtkIdType, call);                 \
-  vtkTemplateMacroCase(VTK_LONG, long, call);                               \
-  vtkTemplateMacroCase(VTK_UNSIGNED_LONG, unsigned long, call);             \
-  vtkTemplateMacroCase(VTK_INT, int, call);                                 \
-  vtkTemplateMacroCase(VTK_UNSIGNED_INT, unsigned int, call);               \
-  vtkTemplateMacroCase(VTK_SHORT, short, call);                             \
-  vtkTemplateMacroCase(VTK_UNSIGNED_SHORT, unsigned short, call);           \
-  vtkTemplateMacroCase(VTK_CHAR, char, call);                               \
-  vtkTemplateMacroCase(VTK_SIGNED_CHAR, signed char, call);                 \
-  vtkTemplateMacroCase(VTK_UNSIGNED_CHAR, unsigned char, call)
-
 switch ( input->GetScalarType() )
   {
-  vtkReducedTypeTemplateMacro( Run( static_cast< VTK_TT *>( 0 ), input, output ); );
+  //vtkReducedTypeTemplateMacro( Run( static_cast< VTK_TT *>( 0 ),
+  //input, output ); );
+#ifdef ITK_UCHAR_TYPE
+  case VTK_UNSIGNED_CHAR:
+    Run( static_cast< unsigned char * >( 0 ), input, output );
+    break;
+#endif
+#ifdef ITK_CHAR_TYPE
+  case VTK_CHAR:
+  case VTK_SIGNED_CHAR:
+    Run( static_cast< char * >( 0 ), input, output );
+    break;
+#endif
+#ifdef ITK_USHORT_TYPE
+  case VTK_UNSIGNED_SHORT:
+    Run( static_cast< unsigned short * >( 0 ), input, output );
+    break;
+#endif
+#ifdef ITK_SHORT_TYPE
+  case VTK_SHORT:
+    Run( static_cast< short * >( 0 ), input, output );
+    break;
+#endif
+#ifdef ITK_UINT_TYPE
+  case VTK_UNSIGNED_INT:
+    Run( static_cast< unsigned int * >( 0 ), input, output );
+    break;
+#endif
+#ifdef ITK_INT_TYPE
+  case VTK_INT:
+    Run( static_cast< int * >( 0 ), input, output );
+    break;
+#endif
+#ifdef ITK_ULONG_TYPE
+  case VTK_UNSIGNED_LONG:
+    Run( static_cast< unsigned long * >( 0 ), input, output );
+    break;
+#endif
+#ifdef ITK_LONG_TYPE
+  case VTK_LONG:
+    Run( static_cast< long * >( 0 ), input, output );
+    break;
+#endif
+#ifdef ITK_FLOAT_TYPE
+  case VTK_FLOAT:
+    Run( static_cast< float * >( 0 ), input, output );
+    break;
+#endif
+#ifdef ITK_DOUBLE_TYPE
+  case VTK_DOUBLE:
+    Run( static_cast< double * >( 0 ), input, output );
+    break;
+#endif
 
   default:
     vtkErrorMacro( "Unknown pixel type encountered" );
